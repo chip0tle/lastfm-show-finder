c27870815ee1b0a48c453b670e574b01c31ff680
matthew benedict <citystorms32@gmail.com>
2026-07-10 16:12:28 -0400

    cleanup() drops any tables matching %_temp name pattern

--- src/store.py
+++ src/store.py
@@ -3,6 +3,9 @@ import duckdb
 
 
 def init_table(conn: DuckDBPyConnection) -> None:
+    """
+    Initialize main tracks_flattened table.
+    """
     conn.sql(
         query="""
         CREATE TABLE IF NOT EXISTS tracks_flattened (
@@ -27,6 +30,9 @@ def init_table(conn: DuckDBPyConnection) -> None:
 
 # TODO: flatten & store (add position column)
 def write_json_to_tracks_db(conn: DuckDBPyConnection, json_data) -> None:
+    """
+    Flatten & store json-structured python dict into persistent duckdb.
+    """
     # Bring python dict (json) into duckdb
     conn.execute(
         query="""
@@ -60,5 +66,19 @@ def write_json_to_tracks_db(conn: DuckDBPyConnection, json_data) -> None:
     )
 
 
-# TODO: temp table & logic for PoP updates
-# TODO: cleanup func for dropping _temp tables
+def cleanup(conn: DuckDBPyConnection):
+    """
+    Drop duckdb tables matching '%_temp' pattern
+    """
+    pattern: str = "%_temp"
+    tables_to_drop = conn.execute(
+        query="SELECT table_name FROM duckdb_tables WHERE table_name LIKE ?",
+        parameters=(pattern,),
+    ).fetchall()
+    for (table_name,) in tables_to_drop:
+        drop_query = f'DROP TABLE IF EXISTS "{table_name}" RESTRICT'
+        conn.execute(query=drop_query)
+        print(f"Dropped: {table_name}")
+
+
+# TODO: temp table & view(?) logic for PoP updates
