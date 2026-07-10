b83f355bfbd8eb982f7582b203d67ca1eb433568
matthew benedict <citystorms32@gmail.com>
2026-07-10 15:42:38 -0400

    added db write logic. currently includes temp table DESCRIBEs & updated todos

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import duckdb
 from src.fetch import get_as_json
-from src.store import init_table
+from src.store import init_table, write_json_to_tracks_db
 from dotenv import load_dotenv
 
 load_dotenv()
 
 LASTFM_KEY: str | None = os.getenv(key="LASTFM_API_KEY")
 LASTFM_CHART_URL: str = f"http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key={LASTFM_KEY}&format=json"
+LASTFM_TOPTRACKS_URL: str = f"http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=us&api_key={LASTFM_KEY}&format=json"
 DATABASE_PATH: str = "data/db.duckdb"
 
 
@@ -15,11 +16,17 @@ def main():
     # Request chart data from Last.fm
     chart_data = get_as_json(api_key=LASTFM_KEY, url=LASTFM_CHART_URL)
     print(chart_data)
+    chart_tracklist = chart_data["tracks"]["track"]
 
     # Init database
     with duckdb.connect(database=DATABASE_PATH) as conn:
-        # Write json to database
+        # Init tracks_flattened if not exists
         init_table(conn)
+        # Flatten & store API data
+        write_json_to_tracks_db(conn, json_data=chart_tracklist)
+        # TODO: PoP update & position calcs logic
+        conn.sql(query="DESCRIBE tracks_temp").show()
+        conn.sql(query="DESCRIBE tracks_flattened").show()
 
 
 if __name__ == "__main__":
